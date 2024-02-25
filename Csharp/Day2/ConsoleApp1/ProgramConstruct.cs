8d1b9e3fb8186eb98ac6ad4b3228ea5f054b03ae
nidhisingh24 <nidhisi@infinite.com>
2024-02-25 18:44:29 +0530

    first commir

---
+++ Csharp/Day2/ConsoleApp1/ProgramConstruct.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ProgramConstruct
+    {
+        public void CheckGrades()
+        {
+            char grades;
+            Console.WriteLine("enter your grades O/A/B/C/D");
+            grades = Convert.ToChar(Console.ReadLine());
+            if (grades == 'O')
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if (grades == 'A')
+            {
+                Console.WriteLine("better");
+            }
+            else if (grades == 'B')
+            {
+                Console.WriteLine("good");
+            }
+            else if (grades == 'D')
+            {
+                Console.WriteLine("avarage");
+            }
+            else if (grades == 'D')
+            {
+                Console.WriteLine("Avarage");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+        public void Grade_Check_With_Switch()
+        {
+            char grade;
+            Console.WriteLine("Enter your Grade A/B/C");
+            grade = Convert.ToChar(Console.ReadLine());
+            switch (grade)
+            {
+                case 'O':
+                    Console.WriteLine("outstanding");
+                    break;
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                    Console.WriteLine("Very Good");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("Can Improve");
+                    break;
+                default:
+                    Console.WriteLine("invalid Grade");
+                    break;
+            }
+
+        }
+    }
+}
